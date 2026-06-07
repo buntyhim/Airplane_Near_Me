faf08e296349f3532f645188282c787e92240ffa
Himanshu Yadav <bunty.him@live.com>
2026-06-07 12:32:56 +0530

    Update WiFi credentials in credential.h
    Put your own wifi credentials

--- src/credential.h
+++ src/credential.h
@@ -1,5 +1,3 @@
 #define WIFI_CREDENTIAL_H
-#define WIFI_SSID "Halbach"
-#define WIFI_PASSWORD "himharigeet"
-#define OLLAMA_URL "http://192.168.0.104:11434" 
-#define LLAMACPP_URL "http://192.168.0.104:8080" 
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
